13b2af94889b3617cabbf61bbc7411beb1bc60f1
Joel Ås <joel.as.0808@gmail.com>
2026-04-28 17:24:58 +0200

    Delete hippie_django/setup.sh

--- hippie_django/setup.sh
+++
@@ -1,3 +0,0 @@
-sudo apt-get install -y postgresql postgresql-contrib  
-sudo -u postgres psql -c "CREATE USER hippie WITH PASSWORD 'hippie';"
-sudo -u postgres psql -c "CREATE DATABASE hippie OWNER hippie;"
